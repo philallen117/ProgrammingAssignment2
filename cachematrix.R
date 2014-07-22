4672f4b5d85486c02a937af449c98382e8aa6f18
philallen117 <phil.allen.gm@gmail.com>
2014-07-22 22:35:13 +0100

    For evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cacheMatrix.R
 
-## Write a short comment describing this function
+# makeCacheMatrix returns a list of functions for use with cacheSolve
+#
+# $set			set the value of the matrix
+# $get			get the value of the matrix
+# $setInverse  	set the value of the inverse of the matrix
+# $getInverse	get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(x1) {
+		x <<- x1
+		inv <<- NULL # Blow the cache
+	}
+	get <- function() { x }
+	setInverse <- function(inv1) { inv <<- inv1 }
+	getInverse <- function() { inv }
+	list(set = set,
+		 get = get,
+		 setInverse = setInverse,
+		 getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+# cacheSolve(aCacheMatrix) calculates and caches the mean of aCacheMatrix,
+# which is constructed using makeCacheMatrix.
+# If there is a cached inverse (aCacheMatri$getInverse() not NULL) returns it.
+# If there is a cache miss,
+# 	gets the matrix to be solved using aCacheMatrix$get(),
+#	solves the matrix using solve { base },
+#   caches the inverse using aCacheMatrix$setInverse(inv).
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(aCacheMatrix, ...) {
+	i <- aCacheMatrix$getInverse()
+	if(is.null(i)) {
+		m <- aCacheMatrix$get()
+		i <- solve(m, ...)
+		aCacheMatrix$setInverse(i)
+	} else {
+		message("Using cached data")
+	}
+	i
 }
